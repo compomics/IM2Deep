096e1e26d4aa662e2ea770f274ba73b41c033b56
rodvrees <robbe.devreese@hotmail.com>
2024-08-27 13:36:31 +0200

    fix rebase

--- im2deep/__init__.py
+++ im2deep/__init__.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-"""IM2Deep: Deep learning framework for peptide collisional cross section prediction."""
-
-__version__ = "0.1.7"
-=======
 """IM2Deep: Deep learning framework for peptide collisional cross section prediction."""
 
 __version__ = "0.1.9"
->>>>>>> f84dfbaaf901c1bea31cd73ef70b2a75fce33368
